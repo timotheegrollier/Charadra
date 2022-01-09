47c25249164146a8b2ffc48d09569f5bcf3390d2
Timo <timotheegrollier@gmail.com>
2022-01-09 20:14:26 +0100

    Fix homepage request,fix token,fix front

--- src/Controller/WorldsController.php
+++ src/Controller/WorldsController.php
@@ -12,12 +12,27 @@ class WorldsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(HttpClientInterface $client): Response
+    public function index(): Response
     {
+
+      
+
+        return $this->render('worlds/index.html.twig');
+    }
+
+
+
+    /**
+     * @Route("/worlds", name="app_worlds")
+     */
+    public function worlds(HttpClientInterface $client):Response{
+
+        $token = $this->getParameter('nw_token');
 
         $response = $client->request(
             'GET',
-            'https://firstlight.newworldstatus.com/ext/v1/worlds/charadra',['headers'=>['Authorization'=> 'Bearer FIRSTLIGHTv4.v4.local.JZ69-Lg7yUssuhe56yNY79XjkMvv4aVXQf3T4GpPHPCBkSQkWAgQhpz9bQomwhnNjuXbh3lSOPtjMKBBhtc6ZtTr8Z8SnBA79dZ6PmnFx0btSz7ckBFXZ3E00oBgmnymSEwCxWKkVd7csjFzCg3q8-sptcYhO92tkR1qzLeaxkgaWEj5eHiRJg9HfWYkQ_KDZ-lWOndG1zyIZitDSGLRuEQi7vt1OK6BET5XcfqANqW3_3GjwOiTWvqIONkK0tGZ2UAzjV6aZzVK1c4F--HIUMr29zWdiawg6MGNYy-7E0L2']]
+            'https://firstlight.newworldstatus.com/ext/v1/worlds/charadra',
+            ['headers' => ['Authorization' => $token]]
         );
 
 
@@ -26,14 +41,15 @@ class WorldsController extends AbstractController
         // $statusCode = 200
         $contentType = $response->getHeaders()['content-type'][0];
         // $contentType = 'application/json'
+
         $worlds = $response->getContent();
         // $content = '{"id":521583, "name":"symfony-docs", ...}'
         $worlds = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+            // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
 
 
-        return $this->render('worlds/index.html.twig', compact('worlds'));
+
+        return $this->render('worlds/worlds.html.twig', compact('worlds'));
     }
-
 
 }
